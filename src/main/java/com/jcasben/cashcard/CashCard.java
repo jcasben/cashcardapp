60f974abf30a2730c753c77b4afa1d038110d836
jcasben <jcasben02@gmail.com>
2024-12-18 23:14:47 +0100

    [UPDATE]: added Repository and consequent changes

--- src/main/java/com/jcasben/cashcard/CashCard.java
+++ src/main/java/com/jcasben/cashcard/CashCard.java
@@ -1,3 +1,5 @@
 package com.jcasben.cashcard;
 
-public record CashCard(Long id, Double amount) {}
+import org.springframework.data.annotation.Id;
+
+public record CashCard(@Id Long id, Double amount) {}
